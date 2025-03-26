182914aa93fb83a42e9af6e8a94ddb5e8af1fa73
Slim-Solo <artur.pucnin@gmail.com>
2025-03-26 14:11:01 +0200

    up

--- NB3.js
+++ NB3.js
@@ -15,12 +15,3 @@ console.log(intervalSum(0, 100));
 console.log(intervalSum(574, 815));
 console.log(intervalSum(-50, 50));
 console.log(intervalSum(-70, 30));
-
-
-function multiply(a, b){
-    if (!a || !b || typeof(a) != "number" || typeof(b) != "number") {
-      return 0;
-    }
-    return a * b;
-}
-console.log();
