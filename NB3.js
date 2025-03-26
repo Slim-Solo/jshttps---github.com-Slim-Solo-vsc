a5cfb52cdfc849e3cf9bb0779104e9874200b76f
Slim-Solo <artur.pucnin@gmail.com>
2025-03-26 14:45:58 +0200

    up

--- NB3.js
+++ NB3.js
@@ -15,3 +15,42 @@ console.log(intervalSum(0, 100));
 console.log(intervalSum(574, 815));
 console.log(intervalSum(-50, 50));
 console.log(intervalSum(-70, 30));
+
+
+// 1)
+
+for (let i=1; i<11; i++)
+{
+    console.log(i);
+}
+
+console.log('-----------------');
+
+
+// 2
+
+for (let i=10; i>0; --i)
+{
+    console.log(i);
+}
+
+console.log('-----------------');
+
+
+// 3
+
+for (let i=0; i<22; i+=2)
+{
+    console.log(i);
+    
+}
+
+console.log('-----------------');
+
+
+// 4
+
+for (let i=22; i>0; --i)
+{
+    console.log(i);   
+}
